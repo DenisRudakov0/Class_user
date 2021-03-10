f02bda3944c49d3e051534e5061982a094e12abd
DenisRudakov0 <denis.rudakov.2000.14@gmail.com>
2021-03-10 11:51:31 +0300

    addclass

--- zd.py
+++ zd.py
@@ -1,3 +1,12 @@
 # TODO: create the User class
 # it must support rank, progress, and the inc_progress(rank) method
 
+class User():
+    rank = -8
+    progress = 0
+
+    def inc_progress(lv):
+        pass
+
+
+    
