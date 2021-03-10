12c24b61a05d9ad6cd50bd12c8ea4a82bd38b76b
DenisRudakov0 <denis.rudakov.2000.14@gmail.com>
2021-03-10 16:45:40 +0300

    error_max_fix

--- zd.py
+++ zd.py
@@ -7,23 +7,26 @@ class User():
 
     def inc_progress(lv):
         rank_list = [i for i in range(-8, 9) if i != 0]
-        if rank_list[rank_list.index(lv) + 1] == user.rank:
+        if rank_list[rank_list.index(lv)] > user.rank and user.rank != 8:
+            user.progress += (10 * (rank_list.index(lv) - rank_list.index(user.rank)) * (rank_list.index(lv) - rank_list.index(user.rank)))
+        elif rank_list[rank_list.index(lv)] == user.rank - 1:
             user.progress += 1
         elif rank_list[rank_list.index(lv)] == user.rank:
             user.progress += 3
-        elif rank_list[rank_list.index(lv)] > user.rank:
-            user.progress += (10 * (lv - user.rank) * (lv - user.rank))
         if user.progress >= 100:
             if user.rank != 8:
-                user.rank = rank_list[rank_list.index(user.rank) + (user.progress // 100)]
-                user.progress = user.progress % 100
-        return user.progress
+                if rank_list.index(user.rank) + (user.progress // 100) > len(rank_list):
+                    user.rank = 8
+                else:
+                    user.rank = rank_list[rank_list.index(user.rank) + (user.progress // 100)]
+                    user.progress = user.progress % 100
+        return user.progress, user.rank 
     
 user = User()
 user.rank = -8
 user.progress = 0
-print(User.inc_progress(-8))
-print(User.inc_progress(-5))
+print(User.inc_progress(8))
+
 
 
     
